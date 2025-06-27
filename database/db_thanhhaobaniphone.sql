53f0f7eb9658e461450c438e12b1ebba216e6da2
maivanhao2110 <maivanhao2110@gmail.com>
2025-06-28 00:08:56 +0700

    them database

---
+++ database/db_thanhhaobaniphone.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2025 at 02:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_thanhhaobaniphone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitiet_donhang`
+--
+
+CREATE TABLE `chitiet_donhang` (
+  `maDH` int(11) NOT NULL,
+  `maSP` int(11) NOT NULL,
+  `loaiSP` enum('M?i','C?') NOT NULL,
+  `soLuong` int(11) NOT NULL,
+  `giaBan` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donhang`
+--
+
+CREATE TABLE `donhang` (
+  `maDH` int(11) NOT NULL,
+  `maKH` int(11) NOT NULL,
+  `ngayDat` date NOT NULL,
+  `trangThai` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iphone_cu`
+--
+
+CREATE TABLE `iphone_cu` (
+  `maSP` int(11) NOT NULL,
+  `tenSP` varchar(255) NOT NULL,
+  `moTa` text DEFAULT NULL,
+  `giaBan` bigint(20) NOT NULL,
+  `soLuong` int(11) NOT NULL DEFAULT 0,
+  `tinhTrang` varchar(50) DEFAULT NULL,
+  `hinhAnh` varchar(255) DEFAULT NULL,
+  `dungLuong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `iphone_new`
+--
+
+CREATE TABLE `iphone_new` (
+  `maSP` int(11) NOT NULL,
+  `tenSP` varchar(255) NOT NULL,
+  `moTa` text DEFAULT NULL,
+  `giaBan` bigint(20) NOT NULL,
+  `soLuong` int(11) NOT NULL DEFAULT 0,
+  `tinhTrang` varchar(50) DEFAULT NULL,
+  `hinhAnh` varchar(255) DEFAULT NULL,
+  `dungLuong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `maKH` int(11) NOT NULL,
+  `tenKH` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `sđt` varchar(20) NOT NULL,
+  `diaChi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chitiet_donhang`
+--
+ALTER TABLE `chitiet_donhang`
+  ADD PRIMARY KEY (`maDH`,`maSP`,`loaiSP`);
+
+--
+-- Indexes for table `donhang`
+--
+ALTER TABLE `donhang`
+  ADD PRIMARY KEY (`maDH`),
+  ADD KEY `maKH` (`maKH`);
+
+--
+-- Indexes for table `iphone_cu`
+--
+ALTER TABLE `iphone_cu`
+  ADD PRIMARY KEY (`maSP`);
+
+--
+-- Indexes for table `iphone_new`
+--
+ALTER TABLE `iphone_new`
+  ADD PRIMARY KEY (`maSP`);
+
+--
+-- Indexes for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`maKH`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `donhang`
+--
+ALTER TABLE `donhang`
+  MODIFY `maDH` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `iphone_new`
+--
+ALTER TABLE `iphone_new`
+  MODIFY `maSP` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `maKH` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitiet_donhang`
+--
+ALTER TABLE `chitiet_donhang`
+  ADD CONSTRAINT `chitiet_donhang_ibfk_1` FOREIGN KEY (`maDH`) REFERENCES `donhang` (`maDH`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `donhang`
+--
+ALTER TABLE `donhang`
+  ADD CONSTRAINT `donhang_ibfk_1` FOREIGN KEY (`maKH`) REFERENCES `khachhang` (`maKH`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
